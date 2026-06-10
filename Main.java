9f05552ea89ee5f3e6d6ce410070ab62b75a52d9
yulieth <yherrerac2@unicartagena.edu.co>
2026-06-09 20:39:29 -0500

    Confirmacion de 2da rama M-S

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Ingrese la cantidad de horas: ");
-        double horas = input.nextDouble();
-        double minutos = horas * 60;
-        System.out.println(horas + " horas equivalen a " + minutos + " minutos.");
+        System.out.println("Ingrese la cantidad de minutos que desea convertir a segundos: ");
+        double minutos = input.nextDouble();
+        double segundos = minutos * 60;
+        System.out.println( minutos + " minutos equivalen a " + segundos + "segundos.");
     }
 }
